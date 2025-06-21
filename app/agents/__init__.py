6b3e9f3281e00d100894d51bea3da6be1b23f142
Colin Gould <colin.gould15@gmail.com>
2025-06-21 15:23:05 -0400

    env setup and initial emotion targeting

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -1,1 +1,5 @@
 # LangGraph Agents Package 
+from .topic_extractor import TopicExtractorAgent
+from .emotion_targeting import EmotionTargetingAgent
+
+__all__ = ['TopicExtractorAgent', 'EmotionTargetingAgent'] 
